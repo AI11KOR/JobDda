d7e7080e09d42bacfae0770f624450c8200c148e
[AI11] 김우현 <dngus523@naver.com>
2025-08-16 16:37:14 +0900

    update social

--- backend/passport/googleStrategy.js
+++ backend/passport/googleStrategy.js
@@ -1,48 +1,95 @@
+// const GoogleStrategy = require('passport-google-oauth20').Strategy;
+// const connectDB = require('../config/database');
+// const passport = require('passport');
+// require('dotenv').config();
+
+// passport.use(
+//     new GoogleStrategy(
+//     {
+//         clientID: process.env.GOOGLE_CLIENT_ID,
+//         clientSecret: process.env.GOOGLE_CLIENT_SECRET,
+//         callbackURL: process.env.GOOGLE_CLIENT_URL
+//     },
+//     async (verifyAccessToken, verifyRefreshToken, profile, done) => {
+//         try {
+//             const db = await connectDB();
+
+//           // ✅ 기존에는 이메일만 비교했다면
+//           // const existingUser = await db.collection('user').findOne({ email: profile.emails[0].value });
+
+
+//           // provider까지 조건에 추가
+//           const existingUser = await db.collection('user').findOne({
+//             email: profile.emails[0].value,
+//             provider: 'google',
+//           });
+
+//           if(existingUser) {
+//             return done(null, existingUser);
+//           }
+
+//           // 없으면 새로 생성
+//           const newUser = {
+//             email: profile.emails[0].value,
+//             nickname: profile.displayName || '구글유저',
+//             provider: 'google',
+//             createdAt: new Date(),
+//           };
+
+//           const insertResult = await db.collection('user').insertOne(newUser);
+//           return done(null, { _id: insertResult.insertedId, ...newUser });
+
+
+//       } catch (err) {
+//         return done(err, null);
+//       }
+//     }
+//   )
+// );
+
 const GoogleStrategy = require('passport-google-oauth20').Strategy;
 const connectDB = require('../config/database');
 const passport = require('passport');
 require('dotenv').config();
 
-passport.use(
-    new GoogleStrategy(
-    {
-        clientID: process.env.GOOGLE_CLIENT_ID,
-        clientSecret: process.env.GOOGLE_CLIENT_SECRET,
-        callbackURL: process.env.GOOGLE_CLIENT_URL
-    },
-    async (verifyAccessToken, verifyRefreshToken, profile, done) => {
-        try {
-            const db = await connectDB();
+// <CHANGE> 환경변수가 있을 때만 Google 전략 등록
+if (process.env.GOOGLE_CLIENT_ID && process.env.GOOGLE_CLIENT_SECRET) {
+  passport.use(
+      new GoogleStrategy(
+      {
+          clientID: process.env.GOOGLE_CLIENT_ID,
+          clientSecret: process.env.GOOGLE_CLIENT_SECRET,
+          callbackURL: process.env.GOOGLE_CLIENT_URL
+      },
+      async (verifyAccessToken, verifyRefreshToken, profile, done) => {
+          try {
+              const db = await connectDB();
 
-          // ✅ 기존에는 이메일만 비교했다면
-          // const existingUser = await db.collection('user').findOne({ email: profile.emails[0].value });
+            const existingUser = await db.collection('user').findOne({
+              email: profile.emails[0].value,
+              provider: 'google',
+            });
 
+            if(existingUser) {
+              return done(null, existingUser);
+            }
 
-          // provider까지 조건에 추가
-          const existingUser = await db.collection('user').findOne({
-            email: profile.emails[0].value,
-            provider: 'google',
-          });
+            const newUser = {
+              email: profile.emails[0].value,
+              nickname: profile.displayName || '구글유저',
+              provider: 'google',
+              createdAt: new Date(),
+            };
 
-          if(existingUser) {
-            return done(null, existingUser);
-          }
+            const insertResult = await db.collection('user').insertOne(newUser);
+            return done(null, { _id: insertResult.insertedId, ...newUser });
 
-          // 없으면 새로 생성
-          const newUser = {
-            email: profile.emails[0].value,
-            nickname: profile.displayName || '구글유저',
-            provider: 'google',
-            createdAt: new Date(),
-          };
-
-          const insertResult = await db.collection('user').insertOne(newUser);
-          return done(null, { _id: insertResult.insertedId, ...newUser });
-
-
-      } catch (err) {
-        return done(err, null);
+        } catch (err) {
+          return done(err, null);
+        }
       }
-    }
-  )
-);
+    )
+  );
+} else {
+  console.log('Google OAuth credentials not found - skipping Google strategy');
+}
--- backend/passport/kakaoStrategy.js
+++ backend/passport/kakaoStrategy.js
@@ -1,76 +1,122 @@
+// const KakaoStrategy = require('passport-kakao').Strategy;
+// const passport = require('passport');
+// require('dotenv').config();
+// const connectDB = require('../config/database')
+
+// passport.use(
+//     new KakaoStrategy(
+//     {
+//         clientID: process.env.KAKAO_CLIENT_ID,
+//         clientSecret: process.env.KAKAO_CLIENT_SECRET,
+//         callbackURL: process.env.KAKAO_CLIENT_URL
+// },
+//     async (verifyAccessToken, verifyRefreshToken, profile, done) => {
+//         console.log('카카오 profile 정보:', profile);
+
+//         try {
+//             const db = await connectDB();
+//             const existingUser = await db.collection('user').findOne({
+//                 email: profile._json?.kakao_account.email,
+//                 provider: 'kakao',
+//             });
+
+//             if(existingUser) {
+//                 return done(null, existingUser);
+//             }
+
+//             const newUser = {
+//                 email: profile._json.kakao_account.email,
+//                 nickname: profile.username || profile.displayName || '.',
+//                 provider: 'kakao',
+//                 createdAt: new Date(),
+//             }
+
+            
+//             const insertResult = await db.collection('user').insertOne(newUser);
+//             return done(null, { _id: insertResult.insertedId, ...newUser });
+            
+//         //     const kakaoAccount = profile._json?.kakao_account;
+//         //     const email = kakaoAccount?.email;
+//         //     const nickname = kakaoAccount?.profile?.nickname || profile.displayName;
+
+//         //     if (!email) {
+//         //         return done(new Error('카카오 로그인에 이메일이 없습니다.'), null);
+//         //     }
+
+//         // // 유저가 존재하는 지 확인
+//         // let user = await db.collection('user').findOne({ email, provider: 'kakao' });
+
+//         // // 없으면 새로 저장
+//         // if(!user) {
+//         //     const result = await db.collection('user').insertOne({
+//         //         email,
+//         //         nickname,
+//         //         provider: 'kakao',
+//         //         createdAt: new Date(),
+//         //     });
+//         //     console.log('카카오 사용자 새로 저장됨:', result.insertedId);
+//         //     user = { _id: result.insertedId, email, nickname };
+//         // } else {
+//         //     console.log('기존 카카오 사용자 로그인:', user._id)
+//         // }
+
+//         // console.log('✅ 카카오 로그인 DB 저장 완료:', user);
+
+//         // // 기존 유저면 그대로 넘김
+//         // return done(null, {
+//         //     _id: user._id,
+//         //     email: user.email,
+//         //     nickname: user.nickname,
+//         // })
+
+//         } catch (error) {
+//             return done(error, null);
+//         }   
+//     }
+// ));
+
 const KakaoStrategy = require('passport-kakao').Strategy;
+const connectDB = require('../config/database');
 const passport = require('passport');
 require('dotenv').config();
-const connectDB = require('../config/database')
 
-passport.use(
+// <CHANGE> 환경변수가 있을 때만 Kakao 전략 등록
+if (process.env.KAKAO_CLIENT_ID && process.env.KAKAO_CLIENT_SECRET) {
+  passport.use(
     new KakaoStrategy(
-    {
+      {
         clientID: process.env.KAKAO_CLIENT_ID,
         clientSecret: process.env.KAKAO_CLIENT_SECRET,
-        callbackURL: process.env.KAKAO_CLIENT_URL
-},
-    async (verifyAccessToken, verifyRefreshToken, profile, done) => {
-        console.log('카카오 profile 정보:', profile);
-
+        callbackURL: process.env.KAKAO_CLIENT_URL,
+      },
+      async (accessToken, refreshToken, profile, done) => {
         try {
-            const db = await connectDB();
-            const existingUser = await db.collection('user').findOne({
-                email: profile._json?.kakao_account.email,
-                provider: 'kakao',
-            });
+          const db = await connectDB();
 
-            if(existingUser) {
-                return done(null, existingUser);
-            }
+          const existingUser = await db.collection('user').findOne({
+            email: profile._json.kakao_account.email,
+            provider: 'kakao',
+          });
 
-            const newUser = {
-                email: profile._json.kakao_account.email,
-                nickname: profile.username || profile.displayName || '.',
-                provider: 'kakao',
-                createdAt: new Date(),
-            }
+          if (existingUser) {
+            return done(null, existingUser);
+          }
 
-            
-            const insertResult = await db.collection('user').insertOne(newUser);
-            return done(null, { _id: insertResult.insertedId, ...newUser });
-            
-        //     const kakaoAccount = profile._json?.kakao_account;
-        //     const email = kakaoAccount?.email;
-        //     const nickname = kakaoAccount?.profile?.nickname || profile.displayName;
+          const newUser = {
+            email: profile._json.kakao_account.email,
+            nickname: profile.displayName || '카카오유저',
+            provider: 'kakao',
+            createdAt: new Date(),
+          };
 
-        //     if (!email) {
-        //         return done(new Error('카카오 로그인에 이메일이 없습니다.'), null);
-        //     }
-
-        // // 유저가 존재하는 지 확인
-        // let user = await db.collection('user').findOne({ email, provider: 'kakao' });
-
-        // // 없으면 새로 저장
-        // if(!user) {
-        //     const result = await db.collection('user').insertOne({
-        //         email,
-        //         nickname,
-        //         provider: 'kakao',
-        //         createdAt: new Date(),
-        //     });
-        //     console.log('카카오 사용자 새로 저장됨:', result.insertedId);
-        //     user = { _id: result.insertedId, email, nickname };
-        // } else {
-        //     console.log('기존 카카오 사용자 로그인:', user._id)
-        // }
-
-        // console.log('✅ 카카오 로그인 DB 저장 완료:', user);
-
-        // // 기존 유저면 그대로 넘김
-        // return done(null, {
-        //     _id: user._id,
-        //     email: user.email,
-        //     nickname: user.nickname,
-        // })
-
-        } catch (error) {
-            return done(error, null);
-        }   
-    }
-));
+          const insertResult = await db.collection('user').insertOne(newUser);
+          return done(null, { _id: insertResult.insertedId, ...newUser });
+        } catch (err) {
+          return done(err, null);
+        }
+      }
+    )
+  );
+} else {
+  console.log('Kakao OAuth credentials not found - skipping Kakao strategy');
+}
